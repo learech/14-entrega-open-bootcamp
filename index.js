c2a053032a9515488342e712e18d18fa11aeb3cb
Lea Rech <learech@hotmail.com>
2023-02-08 19:18:58 -0300

    subiendo

---
+++ index.js
@@ -0,0 +1,10 @@
+const boton = document.querySelector("button")
+
+boton.addEventListener("click", () => {
+    alert("Click en el botón")
+})
+
+
+    $("button").click(function () {
+        console.log("Hola, estoy usando jquery");
+    })
